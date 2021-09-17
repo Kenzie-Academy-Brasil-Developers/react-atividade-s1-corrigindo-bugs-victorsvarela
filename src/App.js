78f313c3c15cd753da6ff56cd03bf0e6851af846
Victor Varela <vitaotapioqueiro@gmail.com>
2021-09-16 23:19:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,53 @@
+import { useEffect, useState } from "react";
 import CardsList from "./components/cards-list";
-import useEffect from "react";
 import "./App.css";
 
+
+
 export const App = () => {
+
   const [showDeck, setShowDeck] = useState(false);
   const [cardsList, setCardsList] = useState([]);
   const [deck, setDeck] = useState("");
 
+
   const handleDeckRequest = () => {
     fetch("https://deckofcardsapi.com/api/deck/new/")
       .then((res) => res.json())
-      .then((res) => console.log(res));
+      .then((res) => setDeck(res));
   };
 
   const handleCardsRequest = (deckId) => {
-    fetch("https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52")
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`)
       .then((res) => res.json())
       .then((res) => setCardsList([...res.cards]));
   };
 
   const handleShowDeck = () => {
-    setShowDeck(false);
+    setShowDeck(!showDeck);
   };
 
   useEffect(() => {
     handleDeckRequest();
-  }, [deck]);
+  }, []);
 
   useEffect(() => {
-    if (deck) handleCardsRequest(deck);
-  }, []);
+    if (deck) {
+      handleCardsRequest(deck["deck_id"]);
+    }
+  }, [deck]);
+
 
   return (
     <div className="main-container">
       <h1 className="main-title">Debugue para ver o baralho</h1>
-      <button onChange={handleShowDeck} className="new-deck-button">
+      <button onClick={() => handleShowDeck()} className="new-deck-button">
         Novo baralho
       </button>
-      {ShowDeck & <CardsList cardsList={cardsList} />}
+      {showDeck && <CardsList cardsList={cardsList} />}
     </div>
   );
 };
+
+
+export default App;
--- src/components/cards-list/index.jsx
+++ src/components/cards-list/index.jsx
@@ -1,70 +1,74 @@
 import { useState } from "react";
-import { Card } from "../card";
+import Card from "../card";
 import "./style.css";
 
+
 const CardsList = ({ cardsList }) => {
+
   const [suitFilter, setSuitFilter] = useState(false);
 
   const handleSuitFilter = (e) => {
-    if (e.target.id = suitFilter) {
-      suitFilter = true;
+    if (e.target.id === suitFilter) {
+      setSuitFilter(false);
       e.target.checked = false;
     } else {
-      suitFilter = e.target.id
+      setSuitFilter(e.target.id)
     }
   };
 
   return (
-    <div className="filter-container">
-      <p>Filtrar por naipe</p>
-      <div>
-        <input
-          onClick={handleSuitFilter}
-          type="radio"
-          id="SPADES"
-          name="suit"
-        />
-        <label for="SPADES">Espadas</label>
-
-        <input
-          onClick={handleSuitFilter}
-          type="radio"
-          id="HEARTS"
-          name="suit"
-        />
-        <label for="HEARTS">Copas</label>
-
-        <input
-          onClick={handleSuitFilter}
-          type="radio"
-          id="CLUBS"
-          name="suit"
-        />
-        <label for="CLUBS">Paus</label>
-
-        <input
-          onClick={handleSuitFilter}
-          type="radio"
-          id="DIAMONDS"
-          name="suit"
-        />
-        <label for="DIAMONDS">Ouros</label>
-      </div>
-    </div>
     <div>
+      <div className="filter-container">
+        <p>Filtrar por naipe</p>
+        <div>
+          <input
+            onClick={(evt) => handleSuitFilter(evt)}
+            type="radio"
+            id="SPADES"
+            name="suit"
+          />
+          <label htmlFor="SPADES">Espadas</label>
 
-      {!suitFilter &&
-        cardsList.map((actual, index) => {
-          return <Card card={actual} key={index} />;
-        })}
+          <input
+            onClick={(evt) => handleSuitFilter(evt)}
+            type="radio"
+            id="HEARTS"
+            name="suit"
+          />
+          <label htmlFor="HEARTS">Copas</label>
 
-      {suitFilter &&
-        cardsList
-          .filter((actual) => actual.suit === suitFilter)
-          .map((actual, index) => {
-            <Card card={actual} key={index} />;
+          <input
+            onClick={(evt) => handleSuitFilter(evt)}
+            type="radio"
+            id="CLUBS"
+            name="suit"
+          />
+          <label htmlFor="CLUBS">Paus</label>
+
+          <input
+            onClick={(evt) => handleSuitFilter(evt)}
+            type="radio"
+            id="DIAMONDS"
+            name="suit"
+          />
+          <label htmlFor="DIAMONDS">Ouros</label>
+        </div>
+      </div>
+      <div>
+        {!suitFilter &&
+          cardsList.map((actual, index) => {
+            return <Card card={actual} key={index} />;
           })}
+
+        {suitFilter &&
+          cardsList
+            .filter((actual) => actual.suit === suitFilter)
+            .map((actual, index) => {
+              return <Card card={actual} key={index} />;
+            })}
+      </div>
     </div>
   );
 };
 
+export default CardsList
